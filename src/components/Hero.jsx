333c7abed44343599d711578f097928bb243b54c
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2026-06-11 12:23:49 +0530

    project start

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+      <h1>Hello, I'm Sachintha</h1>
+
+      <h2>Full Stack Developer</h2>
+
+      <p>
+        Java Spring Boot | React | React Native | MySQL
+      </p>
+
+      <button>Download CV</button>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>Sachintha</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
